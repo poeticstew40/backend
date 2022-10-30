4469575f9d327a0841e3f7e8ce250e6b0ffaae7a
poeticstew40 <nicolas_gigena@hotmail.es>
2022-10-30 16:10:46 -0300

    k

---
+++ src/main/java/com/portfolio/ngd/NgdApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ngd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/ngd/NgdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ngd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
